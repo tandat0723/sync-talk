56f61729f7aa6fce0dd342844eea7cee59f37cda
Tran Tan Dat <trantandat0723@gmail.com>
2026-01-31 02:33:32 +0700

    #4 create api auth to controller signin and signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './libs/db.js'
+import authRoute from './routes/authRoute.js'
 
 dotenv.config()
 
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 5001
 
 //middlewares
 app.use(express.json())
+
+//public routes
+app.use('/api/auth', authRoute)
+
+//private routes
 
 connectDB().then(() => {
     app.listen(PORT, () => {
